55a7135ba5e427c228134bd59aa96f05b1aabc11
mcox5 <mcox5@uc.cl>
2023-09-15 16:38:44 -0300

    feat(sidekiq): change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,8 @@ threads min_threads_count, max_threads_count
 # terminating a worker in development environments.
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "production"
+
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -20,6 +22,9 @@ port ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
