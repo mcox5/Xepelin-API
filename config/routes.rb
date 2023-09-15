b464fdbba732fe05ac614a3796ece81880581bb7
mcox5 <mcox5@uc.cl>
2023-09-15 14:02:39 -0300

    feat(sidekiq): add sidekiq gems and configure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
   get 'articles/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,4 +7,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :articles, only: [:index]
   post 'scrapping', to: 'articles#scrapping'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
